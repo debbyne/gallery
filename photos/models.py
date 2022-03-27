0db2fd65ba4faac9f66c9689f3d823a32855a036
debbyne <kinyadebbyne@gmail.com>
2022-03-27 15:19:03 +0300

    add test for category

--- photos/models.py
+++ photos/models.py
@@ -15,6 +15,7 @@ class Location(models.Model):
     def delete_location(self):
 
         self.delete()
+
     @classmethod
     def update_location(cls ,id ,name):
       return cls.objects.filter(id = id).update(name = name)
@@ -40,8 +41,8 @@ class Images(models.Model):
     image = models.ImageField(upload_to = 'photos/',default="",null=True)
     name = models.CharField(max_length=50,null=True)
     description = models.CharField(max_length=100,null=True)
-    # location = models.ForeignKey(Location,on_delete=models.RESTRICT,)
-    # category = models.ForeignKey(Category, on_delete=models.RESTRICT,)
+    location = models.ForeignKey(Location,on_delete=models.RESTRICT,)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT,)
 
 
     def __str__(self):
@@ -59,11 +60,16 @@ class Images(models.Model):
     @classmethod
     def get_image_by_id(cls,id):
         return cls.objects.filter(id = id).all
-    # @classmethod
-    # def search_image_by_id(cls,search_term):
-    #     try:
-    #     category = Images.objects.get(id = image_id)
-    # except ObjectDoesNotExist:
-    #     raise Http404()
-    # return images
+    @classmethod
+    def search_image_by_category(cls,category):
+        images = cls.objects.filter(category)
+        return images
+    # try:
+    #     category = Category.objects.get(category_name_icontains = category )
+    # except Category.DoesNotExist:
+    #     return images
+    
+    @classmethod
+    def filter_by_location(cls, location):
+        return cls.objects.filter(location=location).all()
     
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Images,Category,Location
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.test_category= Category(name = 'fashion')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.test_category,Category))
+
+     # Testing Save Method
+    def test_update_category(self):
+        self.test_category.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(Category.objects.all()) ==1)
+
+    def test_delete_category(self):
+        self.category.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(Category.objects.all()) ==1)
