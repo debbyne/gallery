477bcd64cfa052de8e47cf240fc169191933640f
debbyne <kinyadebbyne@gmail.com>
2022-03-27 15:42:16 +0300

    add test for class images

--- photos/tests.py
+++ photos/tests.py
@@ -39,3 +39,33 @@ class LocationTestClass(TestCase):
         self.test_location.delete_location()
         location = Location.objects.all()
         self.assertTrue(len(Location.objects.all()) ==1)
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(name='Nakuru')
+        self.location.save_location()
+        self.category= Category(name = 'fashion')
+        self.category.save_category()
+        self.image=Images(name = 'dress',description = 'masterpiece handstiched for two years',location = 'nakuru',category = 'fashion')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.test_image,Images))
+     
+    def test_save_image(self):
+        self.image.save_image()
+        self.assertTrue(len(Images.objects.all()) == 1)
+    def test_delete_image(self):
+        self.test_image.delete_image()
+        image = Images.objects.all()
+        self.assertTrue(len(Images.objects.all()) ==1)
+    def test_get_image_by_id(self):
+        self.image.save_image()
+        self.assertTrue(len(Images.get_image_by_id(self.image.id))>0)
+
+    def test_filter_by_location(self):
+        self.image.save_image()
+        self.assertTrue(len(Images.filter_by_location(self.Nakuru)) == 1)
+
+    def test_search_image_by_category(self):
+        self.image.save_image()
+        self.assertTrue(len(Images.search_image_by_category(self.fashion)) == 1)
